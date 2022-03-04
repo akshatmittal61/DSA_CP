c5988fda09aeeb9fc076dd01af6bb7ac20cd85fa
akshatmittal61 <akshatmittal2506@gmail.com>
2022-03-05 05:04:39 +0530

    contest 1646

---
+++ codeforces/1646/B_Quality_vs_Quantity.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define _ ios_base::sync_with_stdio(0);
+#define fs cin.tie(0);
+#define ll long long int
+#define pb push_back
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sortall(v) sort(v.begin(), v.end())
+#define sortrev(v) sort(v.end(), v.begin())
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n], r = 0, b = 0;
+    fl(i, n) cin >> a[i];
+    sort(a, a + n);
+    r += a[n - 1];
+    b += a[0] + a[1];
+    if (r > b)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    ll first = 2, last = n - 2;
+    while (first < last)
+    {
+        r += a[last];
+        b += a[first];
+        last--;
+        first++;
+        if (r > b)
+        {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+//===========MAIN BEGIN===========
+
+int main()
+{
+    _ fs
+        ll test;
+    cin >> test;
+    while (test--)
+        solve();
+    return 0;
+}
