782c5a10ba3363c1b3e6f16e43b1649e041ead84
akshatmittal61 <akshatmittal2506@gmail.com>
2022-04-02 19:31:52 +0530

    day 12

---
+++ codeforces/A_Phoenix_and_Balance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define _ ios_base::sync_with_stdio(0);
+#define fs cin.tie(0);
+#define ll long long int
+#define pb push_back
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sortall(v) sort(v.begin(), v.end())
+#define sortrev(v) sort(v.end(), v.begin())
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    ll s = 0, t = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i < n / 2 || i == n)
+            s += pow(2, i);
+        else
+            t += pow(2, i);
+    }
+    cout << s - t;
+}
+
+int main()
+{
+    _ fs
+        ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+        cout << endl;
+    }
+    return 0;
+}
